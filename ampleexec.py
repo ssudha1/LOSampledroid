b8dc7d0d3c499e32f15a4d2d7a887c99ddf7c3b3
ssudha1 <30295945+ssudha1@users.noreply.github.com>
2020-12-07 21:04:50 -0600

    Update ampleexec.py

--- ampleexec.py
+++ ampleexec.py
@@ -1,4 +1,5 @@
-import ample_rbtree as alloc
+#!/usr/bin/python
+import ample_atsafemap as atsafemap
 import ampleLOS as heap
 import ampleParse as art
 import amplevm as jvm2
@@ -13,23 +14,23 @@ if(sys.argv[2]=="LOS"):
 	rAddr = heap.rAddr
 	memList = heap.memList
 	mapList = heap.mapList
-	large_objects_ = heap.getlosSpace(nPath, rAddr)
-	[node_begin, node_tail, nodeSize] = alloc.get_rbtree_header(large_objects_, memList)
-	nodes_dict = alloc.rb_traversal(node_begin, node_tail, nodeSize, memList)
+	large_objects_ = heap.extlosSpace(nPath, rAddr)
+	[node_begin, node_tail, nodeSize] = atsafemap.ext_rbtree_header(large_objects_, memList)
+	nodes_dict = atsafemap.rb_traversal(node_begin, node_tail, nodeSize, memList)
 	for K, V in nodes_dict.items():
 		print K+"\t"+V
-	heap.getLosObjects(nodes_dict, mapList, memList)
+	heap.extLosObjects(nodes_dict, mapList, memList)
 	if len(sys.argv)==3:
 	    print "Get the multimedia data by decoding the address above"
 	elif (sys.argv[3]=="decodelosobject"):
 	    addr = sys.argv[4]
 	    print "@ Address "+addr
             print " The extracted large object details"
-	    heap.getLOSObject(addr, jvm2)
+	    heap.extLOSObject(addr, jvm2)
 	elif (sys.argv[3]=="getarray"):
 	    addr = sys.argv[4]
 	    print "@ Address "+addr
             print "The extracted large object primitive array"
-	    heap.getObject(addr, jvm2)
+	    heap.extObject(addr, jvm2)
 	
 
